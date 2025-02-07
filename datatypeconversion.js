ec39bc019c79cc49104fd38dac8687f3a2a29f2b
ChinmayaKolhe <chinubkolhe@gmail.com>
2025-02-07 17:35:08 +0530

    conversion of datatypes

---
+++ datatypeconversion.js
@@ -0,0 +1,22 @@
+let score="33"
+console.log(typeof score) //string
+let numscore=Number(score)
+console.log(typeof numscore) //number
+console.log(numscore) //33
+
+//"33"=33
+//"33abc"=NaN
+//true=1; false=0
+
+let isLoggedin=1
+let booleanValue=Boolean(isLoggedin)
+console.log(booleanValue) //true
+
+//1=true; 0=false
+//""=false; "abc"=true
+
+let somenum=33
+let stringnum=String(somenum)
+console.log(stringnum) //"33"
+console.log(typeof stringnum) //string
+
