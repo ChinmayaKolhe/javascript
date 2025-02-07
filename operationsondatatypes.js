a985b96b9638ca508ce6d2db93a38028eb334cf2
ChinmayaKolhe <chinubkolhe@gmail.com>
2025-02-07 17:44:49 +0530

    final

---
+++ operationsondatatypes.js
@@ -0,0 +1,31 @@
+let value=3
+let negativevale=-value
+console.log(negativevale) //-3
+
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2/2)
+console.log(2%2)
+console.log(2**2)
+
+let str1="Hello"
+let str2="World"
+let str3=str1+str2
+console.log(str3) //HelloWorld
+
+console.log(1+"2") //12
+console.log(1+2+"3") //33
+console.log("1"+2+3) //123
+
+console.log(+true) //1
+console.log(+false) //0
+
+console.log(+"") //0
+
+let gamecounter=100
+gamecounter++
+console.log(gamecounter) //101
+
+gamecounter--
+console.log(gamecounter) //100
