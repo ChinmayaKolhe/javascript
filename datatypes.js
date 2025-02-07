2d0f3d35e5ca923f42b861aaed9bf8ad085e8233
ChinmayaKolhe <chinubkolhe@gmail.com>
2025-02-07 17:26:12 +0530

    final

---
+++ datatypes.js
@@ -0,0 +1,22 @@
+let name="Chinmaya" //string
+let age=21 //number
+let isMale=true //boolean
+let city=null //null
+let country=undefined //undefined
+let person={name:"Chinmaya",age:21} //object
+let hobbies=["reading","coding","gaming"] //array
+let dob=new Date() //date
+let x //undefined
+let y=null //null
+console.log(typeof name)
+console.log(typeof age)
+console.log(typeof isMale)
+console.log(typeof city)
+console.log(typeof country)
+console.log(typeof person)
+console.log(typeof hobbies)
+console.log(typeof dob)
+console.log(typeof x)
+console.log(typeof y)
+
+console.log(typeof null)  //object
