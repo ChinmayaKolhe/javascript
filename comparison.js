90f3f45cf6c1176009a706dc4177c0ddabdf4e23
ChinmayaKolhe <chinubkolhe@gmail.com>
2025-02-08 12:53:09 +0530

    final

---
+++ comparison.js
@@ -0,0 +1,17 @@
+console.log("2">1);
+console.log(null>0); //false
+console.log(null==0); //false
+console.log(null>=0); //true
+console.log(undefined==0); //false
+console.log(undefined>0); //false   
+console.log(undefined<0); //false
+//this comparisons are not possible we avoid them
+//=== is used to compare the values and the data types
+console.log(null===0); //false
+console.log(undefined===0); //false
+console.log(undefined===null); //false
+console.log(null===undefined); //false
+console.log(null===null); //true
+console.log(undefined===undefined); //true
+console.log(0===0); //true
+console.log(0===null); //false
