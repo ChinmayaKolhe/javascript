07a1b95c9f69a7e4e4dc0dca4ae5fac378f58d45
ChinmayaKolhe <chinubkolhe@gmail.com>
2025-02-10 20:45:58 +0530

    Final commit

--- functions.js
+++ functions.js
@@ -41,3 +41,38 @@ console.log(loginMsg);
 
 const loginMsgTwo = loginUserMsg(); //Rohit is logged in
 console.log(loginMsgTwo);
+
+
+/***************************** */
+function calculateCartPrice(val1,val2,...num1){     //Rest operator. It is used to pass a variable number of arguments to a function. 
+    return num1;
+}
+const cartPrice = calculateCartPrice(200,400,500,6000); //200 =val1, 400=val2, 500,6000=num1
+console.log(cartPrice);
+
+
+
+function calculateCartPriceSecond(...num1){     //Rest operator. It is used to pass a variable number of arguments to a function. 
+    return num1;
+}
+const cartPriceTwo = calculateCartPriceSecond(100,200,300,400); 
+console.log(cartPriceTwo);
+
+//functions with objects
+const user={
+    username:"Virat",
+    age:35,
+}
+function handleobj(anyobj){
+    console.log(`username is ${anyobj.username} and age is ${anyobj.age}`);
+}
+handleobj(user);
+handleobj({username:"Rohit",age:36});
+
+
+//functions with array
+const mynewArray=[100,200,300,400,500]
+function returnSecondValue(getArray){
+    return getArray[1];
+}
+console.log(returnSecondValue(mynewArray)); //200
