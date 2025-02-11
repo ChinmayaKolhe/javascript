176cc6030a4b570b12763b80fee75e3e11f6a043
ChinmayaKolhe <chinubkolhe@gmail.com>
2025-02-11 15:03:27 +0530

    final scope prg

--- scope.js
+++ scope.js
@@ -12,4 +12,44 @@ if (true){
     console.log(a,b,c) //block scope
 }
 
-console.log(a,b,c) //global scope
+console.log(a,b,c) //global scope  //value of c is changed to 300 in global scope
+
+//Nested Scope
+
+function one(){
+    const username="chinmaya"
+    function two(){
+        const website="github"
+        console.log(username,website);   
+    }
+    //console.log(website); //website is not defined
+    two()
+}
+one()
+
+if(true){
+    const username="chinmaya"
+    if(username==="chinmaya"){
+        const website="github"
+        console.log(username+" "+website);   
+    }
+    //console.log(website); //website is not defined
+}
+//console.log(username); //username is not defined
+
+
+//************************************************ */
+//Hoisting
+console.log(addone(5)); //function declaration is hoisted
+function addone(value){
+    return value+1
+}
+//const name=addone(6)   
+//console.log(name);
+
+//Function Declaration
+//console.log(addTwo(5));    //function declaration is hoisted
+const addTwo=function(num){
+    return num+2
+}
+console.log(addTwo(5));
